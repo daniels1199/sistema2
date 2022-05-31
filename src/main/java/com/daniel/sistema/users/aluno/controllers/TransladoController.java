8ee9131c35bf79b430ff87722ba2d3a24fd14e28
Daniel Soares <ds507556@gmail.com>
2022-05-31 09:05:09 -0300

    Fazendo atualizações na pagina de cadastro

--- src/main/java/com/daniel/sistema/users/aluno/controllers/TransladoController.java
+++ src/main/java/com/daniel/sistema/users/aluno/controllers/TransladoController.java
@@ -4,6 +4,7 @@ import com.daniel.sistema.users.aluno.repositories.TransladoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TransladoController {
@@ -11,5 +12,9 @@ public class TransladoController {
     @Autowired
     TransladoRepository repo;
 
+    @GetMapping("aluno")
+    public String index(){
+        return "aluno/index";
+    }
     
 }
